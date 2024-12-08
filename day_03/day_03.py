4273379ff5459a421d4a3b2dc53722203a15ab69
simona <simona.musilova11@gmail.com>
2024-12-08 23:55:31 +0100

    p2

--- day_03/day_03.py
+++ day_03/day_03.py
@@ -1,43 +1,43 @@
 import re
 
 
-
-
 def multiply(command: str) -> int:
     nums: list[str] = re.findall(r'[0-9]+', command)
     return int(nums[0]) * int(nums[1])
 
 
-
-
 if __name__ == '__main__':
-    # input_data = "data_test.in"
+    # input_data = "data_test_02.in"
     input_data = "data_challenge.in"
 
     part1: int = 0
     part2: int = 0
-
+    enabled: bool = True
+    instructions: list[tuple] = []
 
     with open(input_data, 'r') as f:
         for line in f.read().splitlines():
-            print(line)
+            # print(line)
 
+            instructions += [(f.start(), f.group()) for f in re.finditer(r'mul\([0-9]{1,3},[0-9]{1,3}\)', line)]
+            instructions += [(f.start(), f.group()) for f in re.finditer(r'do\(\)', line)]
+            instructions += [(f.start(), f.group()) for f in re.finditer(r'don\'t\(\)', line)]
 
-            xx = re.findall(r'mul\([0-9]{1,3},[0-9]{1,3}\)', line)
-            print(xx)
+            instructions.sort(key=lambda x: x[0])
 
+            for instr in instructions:
+                curr: str = instr[1]
 
-            for x in xx:
-                part1 += multiply(x)
-
-            # break
-
-
+                if curr.startswith("do("):
+                    enabled = True
+                elif curr.startswith("don"):
+                    enabled = False
+                else:
+                    tmp = multiply(curr)
+                    part1 += tmp
+                    part2 += tmp if enabled else 0
 
     print("\n-- Part 1: --")
     print(part1)
     print("\n-- Part 2: --")
     print(part2)
-
-
-
