78ffe70ab69d0e64afae3e38b90327dfa08c0095
simona <simona.musilova11@gmail.com>
2024-12-08 23:59:24 +0100

    day 3

--- day_03/day_03.py
+++ day_03/day_03.py
@@ -7,7 +7,6 @@ def multiply(command: str) -> int:
 
 
 if __name__ == '__main__':
-    # input_data = "data_test_02.in"
     input_data = "data_challenge.in"
 
     part1: int = 0
@@ -16,26 +15,24 @@ if __name__ == '__main__':
     instructions: list[tuple] = []
 
     with open(input_data, 'r') as f:
-        for line in f.read().splitlines():
-            # print(line)
+        line = f.read()
 
-            instructions += [(f.start(), f.group()) for f in re.finditer(r'mul\([0-9]{1,3},[0-9]{1,3}\)', line)]
-            instructions += [(f.start(), f.group()) for f in re.finditer(r'do\(\)', line)]
-            instructions += [(f.start(), f.group()) for f in re.finditer(r'don\'t\(\)', line)]
+        instructions += [(f.start(), f.group()) for f in re.finditer(r'mul\([0-9]{1,3},[0-9]{1,3}\)', line)]
+        instructions += [(f.start(), f.group()) for f in re.finditer(r'do\(\)', line)]
+        instructions += [(f.start(), f.group()) for f in re.finditer(r'don\'t\(\)', line)]
+        instructions.sort(key=lambda x: x[0])
 
-            instructions.sort(key=lambda x: x[0])
+        for instr in instructions:
+            curr: str = instr[1]
 
-            for instr in instructions:
-                curr: str = instr[1]
-
-                if curr.startswith("do("):
-                    enabled = True
-                elif curr.startswith("don"):
-                    enabled = False
-                else:
-                    tmp = multiply(curr)
-                    part1 += tmp
-                    part2 += tmp if enabled else 0
+            if curr.startswith("do("):
+                enabled = True
+            elif curr.startswith("don"):
+                enabled = False
+            else:
+                tmp = multiply(curr)
+                part1 += tmp
+                part2 += tmp if enabled else 0
 
     print("\n-- Part 1: --")
     print(part1)
